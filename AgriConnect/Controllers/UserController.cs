0a4c62a4b5358cc6d00a937bc3b8204abb6edb53
ST10085622 <st10085622@vcconnect.edu.za>
2024-06-03 12:18:01 +0200

    update to authenitication

---
+++ AgriConnect/Controllers/UserController.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using AgriEnergy.Models; //importing the models to access the properties
+
+namespace AgriEnergy.Controllers
+{
+    public class UserController : Controller
+    {
+        // GET: Account/Login
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        // POST: Account/Login
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+
+            // Here Im checking the credentials against my database, agri-energy
+            bool isAuthenticated = AuthenticateUser(model.Username, model.Password);
+
+            if (isAuthenticated)
+            {
+                // If authenticated, redirect to another page or perform other actions
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("", "Invalid username or password.");
+                return View(model);
+            }
+        }
+
+        //this where i authenticate the user(s), the employee and farmers combined
+        private bool AuthenticateUser(string username, string password)
+        {
+            
+            return false;
+
+
+        }
+    }
+}
---
+++ AgriConnect/Models/LoginModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace AgriEnergy.Models
+{
+    public class LoginModel
+    {
+        
+        [Required] //its a mandotory string property that the application requires to run
+        public string Username { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]  ///this annotation tells the system to treat this string as a password type, 'hiding'it 
+        public string Password { get; set; }
+
+    }
+}
